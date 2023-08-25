2f7ecf5d1f8a6ced4cae0740c915e5ebca00013d
Kevin Cook <140981683+Kevin-T-Cook@users.noreply.github.com>
2023-08-25 15:40:37 -0700

    Add files via upload
    Made Changes to html, css, and javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Use the API_URL variable to make fetch requests to the API.
 // Replace the placeholder with your cohort name (ex: 2109-UNF-HY-WEB-PT)
-const cohortName = "YOUR COHORT NAME HERE";
+const cohortName = "2307-fsa-et-web-ft-sf";
 const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
 
 /**
@@ -9,7 +9,10 @@ const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}`;
  */
 const fetchAllPlayers = async () => {
   try {
-    // TODO
+    const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2307-fsa-et-web-sf/players")
+    const data = await response.json();
+    console.log(data.data.players)
+    return data.data.players;
   } catch (err) {
     console.error("Uh oh, trouble fetching players!", err);
   }
@@ -22,11 +25,15 @@ const fetchAllPlayers = async () => {
  */
 const fetchSinglePlayer = async (playerId) => {
   try {
-    // TODO
+    const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2307-fsa-et-web-sf/players/"+playerId)
+    const data = await response.json();
+    return data.data.player;
   } catch (err) {
     console.error(`Oh no, trouble fetching player #${playerId}!`, err);
   }
 };
+
+fetchSinglePlayer(742);
 
 /**
  * Updates `<main>` to display a list of all players.
@@ -47,9 +54,40 @@ const fetchSinglePlayer = async (playerId) => {
  * Note: this function should replace the current contents of `<main>`, not append to it.
  * @param {Object[]} playerList - an array of player objects
  */
+
+const main = document.getElementById("main");
+
 const renderAllPlayers = (playerList) => {
-  // TODO
-};
+
+    playerList.forEach(player => {
+     
+      const puppyContainer = document.createElement("div")
+      const nameEle =  document.createElement("h1");
+      const idEle =  document.createElement("h1");
+      const imageEle =  document.createElement("img");
+      const buttonDetails = document.createElement("button");
+      buttonDetails.addEventListener("click", async () => {
+        renderSinglePlayer(await fetchSinglePlayer(player.id));
+      });
+      const buttonDelete = document.createElement("button");
+      buttonDelete.addEventListener("click", () => {
+        main.removeChild(puppyContainer);
+      });
+
+      imageEle.src = player.imageUrl;
+      nameEle.innerHTML = player.name;
+      idEle.innerHTML = player.id;
+      buttonDetails.innerHTML= "SEE DETAILS"
+      buttonDelete.innerHTML = "REMOVE FROM ROSTER"
+
+      puppyContainer.appendChild(nameEle);
+      puppyContainer.appendChild(idEle);
+      puppyContainer.appendChild(imageEle);
+      puppyContainer.appendChild(buttonDetails)
+      puppyContainer.appendChild(buttonDelete)
+      main.appendChild(puppyContainer);
+    })
+  };
 
 /**
  * Updates `<main>` to display a single player.
@@ -65,7 +103,41 @@ const renderAllPlayers = (playerList) => {
  * @param {Object} player an object representing a single player
  */
 const renderSinglePlayer = (player) => {
-  // TODO
+  console.log(player)
+  while(main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
+  const puppyContainer = document.createElement("div")
+  const nameEle =  document.createElement("h4");
+  const idEle =  document.createElement("h4");
+  const breedEle = document.createElement("h4")
+  const imageEle =  document.createElement("img");
+  const teamEle = document.createElement("h4");
+  const buttonReturn = document.createElement("button");
+  buttonReturn.addEventListener("click", () => {
+    window.location.href = "http://127.0.0.1:3000/block23A_CareerSimulation2/puppy-bowl/index.html"
+  }) 
+
+  nameEle.textContent = "Named: ";
+  nameEle.innerHTML += player.name;
+  idEle.textContent = "ID: "
+  idEle.innerHTML += player.id;
+  breedEle.textContext = "Breed: "
+  breedEle.innerHTML += player.breed
+  imageEle.src=player.imageUrl;
+  teamEle.textContent = "Team: "
+  teamEle.innerHTML += player.team.name;
+  buttonReturn.innerHTML = "RETURN TO ROSTER"
+
+  puppyContainer.appendChild(nameEle);
+  puppyContainer.appendChild(idEle);
+  puppyContainer.appendChild(breedEle);
+  puppyContainer.appendChild(teamEle);
+  puppyContainer.appendChild(imageEle);
+  puppyContainer.appendChild(buttonReturn);
+  main.appendChild(puppyContainer);
+
+  // renderSinglePlayer({Name: nameEle, ID: idEle, Breed: breedEle, Team: teamEle}); (this created an infinite loop)
 };
 
 /**
